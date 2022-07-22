21d56522ed6433b578ff81ac390c43b66c59f36d
Chai Ching Ting <joliangel288@gmail.com>
2022-07-22 17:08:55 +0800

    [MOD] AppKey 後端與MIS要調整機器架構｜暫時使用

--- DubaiPalace/Manager/AppKey.swift
+++ DubaiPalace/Manager/AppKey.swift
@@ -7,6 +7,11 @@
 
 public struct AppKey {
     
+    //後端與MIS要調整機器架構｜暫時使用
+//    static let Key = "ndbpp"
+//    static let ApiKey = "a6c0fd2ac1e16d4b"
+//    static let SecretKey = "f75bcf5ab995ae84418be80530ff2328"
+
     static let Key = "ndbpp"
     static let ApiKey = "6d2add3b7474ff72"
     static let SecretKey = "b3f12ec27eca12073641821fbbbb442c"
